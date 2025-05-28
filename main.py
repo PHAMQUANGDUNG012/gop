b987c1597e4b54f2f9185d39b7beae40979b70df
PHẠM QUANG DŨNG <phamquangdung012@gmail.com>
2025-05-28 19:42:45 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,151 @@
+import requests
+import base64
+
+
+
+
+import requests
+
+do = "\033[1;91m"
+xanhbien = "\033[1;36m"
+vang = "\033[0;33m"
+hong = "\033[1;35m"
+xanhduong = "\033[1;20m"
+xanhla = "\033[1;32m"
+xanh="\033[1;32m"
+cam="\033[1;33m"
+blue="\033[1;20m"
+lam="\033[1;20m"
+tim="\033[1;20m"
+syan="\033[1;36m"
+xnhac= "\033[1;96m"
+den="\033[1;90m"
+luc="\033[1;92m"
+xduong="\033[1;94m"
+trang="\033[1;97m"
+den = "\033[1;90m"
+luc = "\033[1;32m"
+trang = "\033[1;37m"
+red = "\033[1;31m"
+do = "\033[1;31m"
+vang = "\033[1;33m"
+tim = "\033[1;35m"
+lamd = "\033[1;20m"
+lam = "\033[1;36m"
+tim = "\033[35m"
+hong = "\033[1;95m"
+
+import os, sys
+try:
+	import requests, json, re, random
+except:
+	os.system("pip install requests")
+	import requests, json, re, random
+from random import randint
+from time import sleep
+from pystyle import Colors, Colorate
+from pystyle import *
+from datetime import datetime
+import uuid
+from random import choice, randint, shuffle
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+from os.path import isfile
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re,requests,os,sys
+from time import sleep 
+from datetime import date
+import requests, random
+import requests
+import base64, json,os
+from datetime import date
+from datetime import datetime
+from time import sleep,strftime
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re,requests,os,sys
+from time import sleep 
+from datetime import date
+import requests, random
+import uuid, re
+from pystyle import Write,Colors
+from bs4 import BeautifulSoup
+import socket
+from datetime import datetime
+System.Clear()
+System.Title("NQ-tool")
+def thanhngang(so):
+    for i in range(so):
+        print(trang+'-',end ='')
+    print('')
+time=datetime.now().strftime("%H:%M:%S")
+from pystyle import *
+data_machine = []
+today = date.today()
+now = datetime.now()
+thu = now.strftime("%A")
+ngay_hom_nay = now.strftime("%d")
+thang_nay = now.strftime("%m")
+nam_ = now.strftime("%Y")
+
+
+thanh = f'{red}[{trang}</>{red}] {trang}=>'
+ip = requests.get('https://kiemtraip.com/raw.php').text
+
+key_time = 'Bạn không ngu, do tôi quá giỏi'
+
+banne=f'''\033[1;36m
+██████╗   ██████╗  ██████╗  ████████╗ ██████╗  ██████╗ ██╗     
+██╔══██╗ ██╔═══██╗ ██╔══██╗ ╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+██████╔╝ ██║   ██║ ██║  ██║    ██║   ██║   ██║██║   ██║██║     
+██╔═══╝  ██║▄▄ ██║ ██║  ██║    ██║   ██║   ██║██║   ██║██║     
+██║      ╚██████╔╝ ██████╔╝    ██║   ╚██████╔╝╚██████╔╝███████╗
+╚═╝        ══▀▀═╝  ╚═════╝     ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
+{trang}-----------------------------------------------------------------
+{thanh} {xanhla}Youtube : {trang}https://youtube.com/@JirayIsMe
+{thanh} {lam}Zalo Box : {trang}https://zalo.me/g/utuzva424
+{thanh} {lam}IP MẠNG : {trang}{ip}
+{thanh} {xanhla}NGÀY : {trang}{time}  
+{trang}-----------------------------------------------------------------
+╔═══════════════════════════╗
+║ TOOL TRAODOISUB FACEBOOK  ║
+╚═══════════════════════════╝
+{thanh} {luc}Nhập {red}[{vang}1{red}] {luc}Tool TDS FACEBOOK
+{thanh} {luc}Nhập {red}[{vang}2{red}] {luc}Tool TDS TIKTOK{trang}
+╔════════════════════════════╗
+║ TOOL TUONGTACCHEO FACEBOOK ║
+╚════════════════════════════╝
+{thanh} {luc}Nhập {red}[{vang}3{red}] {luc}Tool TTC Facebook{trang}
+╔════════════════════════════╗
+║ TOOL TIỆN ÍCH MỚI          ║
+╚════════════════════════════╝ 
+{thanh} {luc}Nhập {red}[{vang}4{red}] {luc}Tool Nuôi Acc Facebook
+{thanh} {luc}Nhập {red}[{vang}5{red}] {luc}Tool Chuyển Page 
+{thanh} {luc}Nhập {red}[{vang}6{red}] {luc}Tool Kết Bạn Facebook
+{trang}-----------------------------------------------------------------'''
+while True:
+	print(banne)
+	chon = str(input(f'{thanh}{luc}Nhập Số \033[1;37m: \033[1;33m'))
+	if chon == 0:
+		exec(requests.get('').text)
+	elif chon == 1 :
+		exec(requests.get('').text)
+	elif chon == 2 :
+		exec(requests.get('').text)
+	elif chon == 3 :
+		exec(requests.get('').text)
+	elif chon == 4 :
+		exec(requests.get('').text)
+	#tien ích
+	elif chon == 5 :
+		exec(requests.get('').text)
+	elif chon == 6 :
+		exec(requests.get('').text)
+	elif chon == 7 :
+		exec(requests.get('').text)
+	elif chon == 8 :
+		exec(requests.get('').text)
+	else:
+		print("Sai Lựa Chọn!!!")
+
+
